77ddac32ecb95e7d409482a7e3e9bdb49daf1993
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-12-23 12:40:03 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
+import VueFbCustomerChat from '../dist/vue-fb-customer-chat.umd.js'
 import App from './App.vue'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-139039966-2'
+})
+
+Vue.use(VueFbCustomerChat, {
+  page_id: 562547237566000,
+  theme_color: '#2c3e50',
+  locale: 'en_US'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: function (h) { return h(App) },
 }).$mount('#app')
